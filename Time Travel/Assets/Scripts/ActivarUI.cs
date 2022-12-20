e13c09f9632afde6b39aedad4dfe8c65435c1eca
Polalf <103451330+Polalf@users.noreply.github.com>
2022-12-20 12:31:44 -0300

    Cnvas

---
+++ Time Travel/Assets/Scripts/ActivarUI.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivarUI : MonoBehaviour
+{
+    public GameObject[] Ui;
+
+    public bool Activar;
+    void Update()
+    {
+       
+    }
+}
